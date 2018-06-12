13fe1abd2c3bdc4c51cfbebca8e3ad3b2d2eeb9b
Murat Celik <cashkl@yandex.com>
2018-06-12 20:13:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $('img').append('class="tasty"');
+}
+
+function pressIt() {
+  alert("Hey you pressed the G");
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
 
 $(document).ready(function(){
 
-// call functions here
+$('p').addEventListener('click',function() {
+  getIt();
+});
+
+$('img').on('load', function() {
+  frameIt();
+});
+$(document).on('keydown',function(key) {
+if(key.which==71){
+pressIt();
+}
+$('form').on('submit',function() {
+  submitIt();
+})
+});
+
+
+
 
 });
